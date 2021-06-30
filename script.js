bd36ac1c0f7c97bb8305cb1f46d18496473b3b71
Roccofranchini <rocco.franchini1@gmail.com>
2021-06-30 14:49:47 +0200

    password

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 /*
 Chiedi nome, Cognome e Colore preferito, infine scrivi nomecognomecolorepreferito21
+
+1- creiamo le variabili nome, cognome e colore e le chiediamo con un prompt
+2- recuperiamo l'elemento html da modificare
+3- modifichiamo l'elemento con le 3 variabili ed il 21
 */
 
+
+var nome = prompt('inserisci il tuo nome')
+var cognome = prompt('inserisci il tuo cognome')
+var colorePreferito = prompt('inserisci il tuo colore preferito')
+
+var password = document.getElementById('password');
+password.innerHTML = nome + cognome + colorePreferito + 21;
